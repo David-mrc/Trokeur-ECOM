191e5c61a8d1607dfc8bf18faefd5584a2a1564f
Evan1700 <evan17.pereira@gmail.com>
2023-09-15 10:12:45 +0200

    FEAT : carousel modal when clicking on image

--- src/main/webapp/app/components/product-card/product-card.component.ts
+++ src/main/webapp/app/components/product-card/product-card.component.ts
@@ -4,19 +4,25 @@ import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TradeObject } from 'app/interfaces/TradeObjectInterface';
 import { StateComponent } from '../state/state.component';
+import { NgbModal, NgbModalRef, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @Component({
   selector: 'jhi-product-card',
   templateUrl: './product-card.component.html',
   standalone: true,
-  imports: [StateComponent, NgIf, FontAwesomeModule, RouterLink],
+  imports: [StateComponent, NgIf, FontAwesomeModule, RouterLink, NgbModule],
   styleUrls: ['./product-card.component.scss']
 })
 
 export class ProductCardComponent {
   @Input() tradeObject: TradeObject | undefined;
   @Input() isPublic = true;
+  private modalRef!: NgbModalRef;
+
+  constructor(public modalService: NgbModal){
+  }
 
   getPathToFirstImage(): string {
     if (this.tradeObject?.genericImages?.[0]) {
@@ -24,5 +30,13 @@ export class ProductCardComponent {
     } else {
       return "/content/images/logoTrokeur.png";
     }
+  }
+
+  openCarousel(content : any): void {
+    this.modalRef = this.modalService.open(content, { centered: true });
+  }
+
+  closeModal() : void {
+    this.modalRef.close('Cross click');
   }
 }
