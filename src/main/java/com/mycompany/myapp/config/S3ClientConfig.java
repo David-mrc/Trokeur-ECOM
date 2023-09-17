9a329bad036d7b61d77d22f8ad84f5768e53198d
Drayzd <g31plus@gmail.com>
2023-09-17 15:26:21 +0200

    BUILD: Use Env variable for secret

--- src/main/java/com/mycompany/myapp/config/S3ClientConfig.java
+++ src/main/java/com/mycompany/myapp/config/S3ClientConfig.java
@@ -6,17 +6,20 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties(prefix = "aws")
 public class S3ClientConfig {
 
-    //@Value("temp")
-    private String accessKey = "AKIA4L3VOIGJQDC7PT6V";
+    @Value("${aws.accessKey}")
+    private String accessKey;
 
-    //@Value("temp")
-    private String secretKey = "cKz7ANpZgHpc/I4VSIjbUdhPNH8BYsNrn9wDUwQ8";
+    @Value("${aws.secretKey}")
+    private String secretKey;
 
     @Bean
     public AmazonS3 amazonS3Client() {
