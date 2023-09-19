4cd0f13bcf1241275a073ad070eb9757db50f2a2
perringe <perringerome.pro@gmail.com>
2023-09-19 15:07:17 +0200

    FEAT : update homepage design

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -6,26 +6,36 @@ import { takeUntil } from 'rxjs/operators';
 import SharedModule from 'app/shared/shared.module';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
+import { ProductCardComponent } from 'app/components/product-card/product-card.component';
+import { NgFor, NgIf } from '@angular/common';
+import { TradeObject } from 'app/interfaces/TradeObjectInterface';
+import { productService } from 'app/components/product-service';
 
 @Component({
   standalone: true,
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [SharedModule, RouterModule],
+  imports: [SharedModule, RouterModule, ProductCardComponent, NgFor, NgIf],
 })
 export default class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
 
   private readonly destroy$ = new Subject<void>();
+  private tradeObjectList: TradeObject[] = [];
 
-  constructor(private accountService: AccountService, private router: Router) {}
+  constructor(private accountService: AccountService, private router: Router, private _productService: productService) {}
 
   ngOnInit(): void {
     this.accountService
       .getAuthenticationState()
       .pipe(takeUntil(this.destroy$))
       .subscribe(account => (this.account = account));
+    this._productService.getAllProductsFromPage(0).subscribe((tradeObjects) => {
+      tradeObjects.map((tradeObject: TradeObject) => {
+        this.tradeObjectList.push(tradeObject);
+      })
+    });
   }
 
   login(): void {
@@ -35,5 +45,9 @@ export default class HomeComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  getTradeObjectList(): TradeObject[] {
+    return this.tradeObjectList;
   }
 }
